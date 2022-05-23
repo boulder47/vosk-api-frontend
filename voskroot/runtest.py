73fac9d3e99972e68c527a26deb03adcb0fbb468
boulder47 <101745098+boulder47@users.noreply.github.com>
2022-05-23 13:49:30 -0500

    Add files via upload

---
+++ voskroot/runtest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+#from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
+import os
+#import wave
+#import subprocess
+#import json
+
+#SetLogLevel(0)
+
+#if not os.path.exists("model"):
+#    print ("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
+#    exit (1)
+
+#sample_rate=16000
+#model = Model("model")
+#rec = KaldiRecognizer(model, sample_rate)
+
+#process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+#                            sys.argv[1],
+#                            '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
+#                            stdout=subprocess.PIPE)
+#
+filename1 = sys.argv[1]
+filename1 = filename1.replace('audio','text')
+sys.stdout = open(filename1+'.txt', 'x')
+
+#while True:
+#data = process.stdout.read(4000)
+#with open("myfile.txt", "w") as file1:
+#if len(data) == 0:
+#        break
+#if rec.AcceptWaveform(data):
+#        res = json.loads(rec.Result())
+print (filename1)
+#print (path1)
+#with open("myfile.txt", "w") as file1:
+# Writing data to a file
+#file1.write(res['text'])
+    
+
+#res = json.loads(rec.FinalResult())
+print ('this is a test')
+#file1.write(res['text'])
+#file1.close()
